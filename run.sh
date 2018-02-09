0e2137b2f6fc6bcb4320a982275d9ea60f483308
Денис Туляков <dtulyakov@gmail.com>
2018-02-09 21:56:29 +0300

    изменено:      run.sh

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ set -x
 HTMLFILE=$(cd ${WORKSPACE} && ls *.html)
 for htmlf in ${HTMLFILE}
     do
-    html-minifier --minify-js uglify-js --minify-css clean-css --keep-closing-slash ${WORKSPACE}/${htmlf} -o ${WORKSPACE/${htmlf}
+    html-minifier --minify-js uglify-js --minify-css clean-css --keep-closing-slash ${WORKSPACE}/${htmlf} -o ${WORKSPACE}/${htmlf}
 done
