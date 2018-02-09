6c6248567d46e8d22bed2ae82fac8bb55f40b054
Денис Туляков <dtulyakov@gmail.com>
2018-02-09 21:18:58 +0300

    новый файл:    run.sh

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+set -x
+HTMLFILE=$(cd ${WORKSPACE} && ls *.html)
+for htmlf in ${HTMLFILE}
+    do
+    html-minifier --minify-js uglify-js --minify-css clean-css --keep-closing-slash ${WORKSPACE}/${htmlf} -o ${WORKSPACE/${htmlf}
+done
